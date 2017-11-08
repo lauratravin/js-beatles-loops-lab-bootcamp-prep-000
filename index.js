6870cdfd1c38d2c0f16f2df87ab5651d251d0f79
Laura Travin <laura.travin@gmail.com>
2017-11-08 20:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+var musicians = ['John Lennon'];
+var instruments = ['guitar'];
+
 function theBeatlesPlay(musicians,instruments){
   var myArray = [];
      var i = myArray.length;
 
      for (let j=0; j<=i; j++){
           myArray.push();
-       
+
      }
 
-  
+
 }
