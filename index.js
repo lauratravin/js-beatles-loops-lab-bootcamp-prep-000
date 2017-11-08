ac90756445d433c20000e606a9c0a7cc196ef744
Laura Travin <laura.travin@gmail.com>
2017-11-08 20:19:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function theBeatlesPlay(){
+function theBeatlesPlay(musicians,instruments){
+  var myArray = [];
+     var i = myArray.length;
+
+     for (let j=0; j<=i; j++){
+          myArray.push();
+       
+     }
 
   
 }
